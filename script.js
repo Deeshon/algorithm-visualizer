770ae16d958f53b523904c03f9cf317b80ff9d4a
Deeshon Hunukumbura <deeshonhunukumbura18@gmail.com>
2023-03-09 22:54:03 +0530

    edited index.html and script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector(".canvas")
-const bars = document.querySelectorAll(".bar")
+
 const newArray = document.querySelector("#new")
 const sort = document.querySelector("#sort")
 
@@ -23,45 +23,6 @@ newArray.addEventListener('click', () => {
     
 })
 
-function swap(el1, el2) {
-    console.log('In swap()');
-    
-    let temp = el1.style.height;
-    let temp_val = el1.textContent
-    el1.style.height = el2.style.height;
-    el2.style.height = temp;
-
-    
-}
-
-function waitforme(milisec) { 
-    return new Promise(resolve => { 
-        setTimeout(() => { resolve('') }, milisec); 
-    }) 
-}
-
-async function bubble() {
-    console.log('In bubbe()');
-    const ele = document.querySelectorAll(".bar");
-    for(let i = 0; i < ele.length-1; i++){
-        console.log('In ith loop');
-        for(let j = 0; j < ele.length-i-1; j++){
-            console.log('In jth loop');
-            ele[j].style.background = '#2a52be';
-            ele[j+1].style.background = '#2a52be';
-            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
-                console.log('In if condition');
-                await waitforme(50);
-                swap(ele[j], ele[j+1]);
-
-            }
-            ele[j].style.background = '#00BFFF';
-            ele[j+1].style.background = '#00BFFF';
-        }
-        ele[ele.length-1-i].style.background = '#5F9EA0';
-    }
-    ele[0].style.background = '#5F9EA0';
-}
 
 
 sort.addEventListener('click', async function(){
